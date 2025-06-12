c242a38357485daeab3c309466913bd916e8eae3
@vanshkansal25 <kansalv578@gmail.com>
2025-06-12 11:42:25 +0530

    Add Like model schema and utility files

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,4 +1,13 @@
 import mongoose,{Schema} from "mongoose";
 const likeSchema = new Schema({
-    
+    article:{
+        type:Schema.Types.ObjectId,
+        ref:"Article"
+    },
+    likedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
 })
+
+export const Like = mongoose.model("Like",likeSchema)
